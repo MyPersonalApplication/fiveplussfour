b5a09d10466c09cdefb01d73bad2967cfa7f7827
quangndgcc200030 <91590378+quangndgcc200030@users.noreply.github.com>
2023-05-18 20:54:44 +0700

    change connectDb

--- connectDB.php
+++ connectDB.php
@@ -1,2 +1,2 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'fiveplussfour') or die("Can not conncect database" . mysqli_connect_error());
+$conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'baeee2c00c416d', '5a617ef1', 'heroku_c0a5db3bebf9031') or die("Can not conncect database" . mysqli_connect_error());
